abf6e5cae5c8c5078feb623e46f9e3feb5f00cd0
lawrence <kofiarkoh0@gmail.com>
2023-06-19 23:04:21 +0000

    add login endpoint

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterUserController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
@@ -17,6 +18,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('auth/register', RegisterUserController::class);
+Route::post('auth/login', LoginController::class);
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
