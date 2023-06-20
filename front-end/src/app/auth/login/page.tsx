323cd56d13a1c2c98483ca5df44efd739ec6ceb0
lawrence <kofiarkoh0@gmail.com>
2023-06-20 20:43:49 +0000

    configure material ui v5

---
+++ front-end/src/app/auth/login/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import Button from "@mui/material/Button";
+import React from "react";
+
+export default function LoginPage() {
+	return (
+		<div>
+			<Button>dsds</Button>
+		</div>
+	);
+}
